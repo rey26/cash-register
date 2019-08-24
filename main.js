3adf7ca979f1bbd1a75a55d237d50322a3e2f3e0
Matej Malicky <matej.malicky@gmail.com>
2019-08-24 18:09:59 +0200

    convert money from word to numeric value

--- main.js
+++ main.js
@@ -13,11 +13,37 @@ function checkCashRegister(price, cash, cid) {
       availableCash += cash[1];
     });
 
-    console.log(availableCash);
-
     if(cash - price > availableCash){
         return {status: "INSUFFICIENT_FUNDS", change: []};
     }
+
+    function convertMoney(cash){
+        switch (cash[0]){
+            case "ONE HUNDRED":
+                return 100;
+            case "TWENTY":
+                return 20;
+            case "TEN":
+                return 10;
+            case "FIVE":
+                return 5;
+            case "ONE":
+                return 1;
+            case "QUARTER":
+                return 0.25;
+            case "DIME":
+                return 0.1;
+            case "NICKEL":
+                return 0.05;
+            case "PENNY":
+                return 0.01;
+            default:
+                return 0; 
+        }
+    }
+
+    console.log(convertMoney(cid[3]));
+
     // Here is your change, ma'am.
     return change;
   }
@@ -32,6 +58,5 @@ function checkCashRegister(price, cash, cid) {
   // ["TEN", 20],
   // ["TWENTY", 60],
   // ["ONE HUNDRED", 100]]
-//   setInterval(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), 1000);
-  setInterval(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ]), 1000);
+  setInterval(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]), 1000);
   
